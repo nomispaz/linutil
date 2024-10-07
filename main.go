51fc6c06b0da24f655ce3775e5d3aceab5db8dda
nomispaz <l0rdp4z1f1st@gmail.com>
2024-10-07 22:04:27 +0200

    Test

--- main.go
+++ main.go
@@ -10,10 +10,34 @@ import (
 	"github.com/rivo/tview"
 )
 
+// Mode - Custom type to hold value for mode of operation
+type Mode int
+
+// Declare related constants for each weekday starting with index 1
+const (
+	None Mode = iota + 1 // EnumIndex = 1
+	Clone
+	Push
+)
+
+// String - Creating common behavior - give the type a String function
+func (m Mode) String() string {
+	return [...]string{"None", "Clone", "Push"}[m-1]
+}
+
+// EnumIndex - Creating common behavior - give the type a EnumIndex function
+func (m Mode) EnumIndex() int {
+	return int(m)
+}
+
+// struct to save some variables that should be accessed from functions
 type State struct {
 	password string
 	command  string
+	mode     Mode
 }
+
+// struct that contains the definition of the Tui (variables and widgets)
 type Tui struct {
 	state State
 
@@ -30,10 +54,12 @@ type Tui struct {
 	password_prompt *tview.InputField
 }
 
+// Initiate the Tui (variables and widgets)
 func (t *Tui) Init() {
 
 	t.state.password = "none"
 	t.state.command = "none"
+	t.state.mode = None
 
 	t.app = tview.NewApplication()
 	t.contents = tview.NewTextView()
@@ -45,16 +71,96 @@ func (t *Tui) Init() {
 	t.password_prompt = tview.NewInputField()
 }
 
+// get mode from list item
+func (t *Tui) GetTextFromListItem() {
+	current_item_text1, _ := t.menu.GetItemText(t.menu.GetCurrentItem())
+
+	switch t.state.mode {
+	case None:
+		// no mode selected --> still the starting menu is shown
+		switch current_item_text1 {
+		case "Clone repos":
+			t.state.mode = Clone
+			c := make(chan string)
+
+			command := "curl https://api.github.com/users/nomispaz/repos | grep full_name | cut -d':' -f 2 | cut -d'\"' -f 2 | cut -d'/' -f 2"
+
+			go execCmd(c, command, "out")
+
+			// remove all items from the list
+			t.menu.Clear()
+
+			// loop through channel of cmd ouput
+			for msg := range c {
+				t.menu.AddItem(msg, "", '-', t.GetTextFromListItem)
+				t.app.ForceDraw()
+
+			}
+		case "Push repos":
+			t.state.mode = Push
+			c := make(chan string)
+
+			command := "ls /home/simonheise/git_repos/"
+
+			go execCmd(c, command, "both")
+
+			// remove all items from the list
+			t.menu.Clear()
+
+			// loop through channel of cmd ouput
+			for msg := range c {
+				t.menu.AddItem(msg, "", '-', t.GetTextFromListItem)
+				t.app.ForceDraw()
+
+			}
+		}
+	case Clone:
+		// mode clone --> if item is selected, it is cloned
+		t.contents.Clear()
+		c := make(chan string)
+		//clone the repo
+		command := fmt.Sprintf("git clone --progress https://github.com/nomispaz/%s /home/simonheise/git_repos/%s", current_item_text1, current_item_text1)
+		go execCmd(c, command, "both")
+		var output = ""
+		// loop through channel of cmd ouput
+		for msg := range c {
+			output += output + "\n" + msg
+			t.contents.SetText(output)
+			t.app.ForceDraw()
+		}
+	case Push:
+		t.contents.Clear()
+		c := make(chan string)
+		//push the repo
+		command := fmt.Sprintf("pushd /home/simonheise/git_repos/%s; git add .; git commit -m \"%s\"; git push; popd", current_item_text1, "Test")
+		go execCmd(c, command, "both")
+		var output = ""
+		// loop through channel of cmd ouput
+		for msg := range c {
+			output += output + "\n" + msg
+			t.contents.SetText(output)
+			t.app.ForceDraw()
+		}
+	}
+}
+
+// setup the interface and functions belonging to the widgets like SetFinishedFunc etc.
 func (t *Tui) SetupTUI() {
 
-	t.contents.SetTextAlign(tview.AlignLeft).SetText("").SetDynamicColors(false).SetTextColor(tcell.ColorSlateGrey)
+	// ScrollToEnd is used so that the list is always scrolled together with the text
+	t.contents.SetTextAlign(tview.AlignLeft).SetText("").SetDynamicColors(false).SetTextColor(tcell.ColorSlateGrey).ScrollToEnd()
 
+	// define password prompt for the sudo password
 	t.password_prompt.SetLabel("Enter root password: ")
 	t.password_prompt.SetFinishedFunc(func(key tcell.Key) {
 		t.state.password = t.password_prompt.GetText()
 		t.pages.SendToFront("flex")
-
 	})
+
+	// hide second row for the menu items
+	t.menu.ShowSecondaryText(false)
+	t.menu.AddItem("Clone repos", "", '-', t.GetTextFromListItem)
+	t.menu.AddItem("Push repos", "", '-', t.GetTextFromListItem)
 
 	// Returns a new primitive which puts the provided primitive in the center and
 	// sets its size to the given width and height.
@@ -68,45 +174,55 @@ func (t *Tui) SetupTUI() {
 			AddItem(nil, 0, 1, false)
 	}
 
+	// define the layout of the main window
 	t.flexLeftCol.SetDirection(tview.FlexRow)
 	t.flexLeftCol.AddItem(t.menu, 0, 100, true).SetBorder(true)
 	t.flexRightCol.SetDirection(tview.FlexRow)
 	t.flexRightCol.AddItem(t.contents, 0, 100, false).SetBorder(true)
 
 	t.flex.SetDirection(tview.FlexColumn).
-		AddItem(t.flexLeftCol, 0, 1, false).
+		AddItem(t.flexLeftCol, 0, 1, true).
 		AddItem(t.flexRightCol, 0, 3, false)
 
+	// define pages so that we are able to switch between main layout and popups
 	t.pages.
 		AddPage("password", modal(t.password_prompt, 40, 10), true, true).
 		AddPage("flex", t.flex, true, true)
 
+	// start the app with the main layout shown
 	t.pages.SendToFront("flex")
 }
 
+// wrapper to execute comands that require a sudo password
 func (t *Tui) handle_sudo_cmd() {
+	// show the password prompt
 	t.pages.SendToFront("password")
 	t.app.ForceDraw()
 
 	// wait for password to be entered
 	go func() {
 		for {
+			// if the password was entered, continue
 			if t.state.password != "none" {
 
 				var c = make(chan string)
-				go execCmd(c, fmt.Sprintf("echo %s | sudo -S ls -l", t.state.password))
-				//go execCmd(c, "pushd /home/simonheise/git_repos/nompac/overlays/tuxedo-drivers-dkms/; makepkg -cCsrf --skippgpcheck; popd")
+				// execute the command and feed password to command. The command returns each outut line via channel
+				go execCmd(c, fmt.Sprintf("echo %s | sudo -S ls -l", t.state.password), "both")
+
 				var output = ""
 
+				// loop through channel of cmd ouput
 				for msg := range c {
 
 					if msg == "[sudo] password for root: Sorry, try again." {
+						// the password was not correct
 						t.contents.SetText("Password was not correct, retry.")
 						t.state.password = "none"
 						t.app.ForceDraw()
 						break
 
 					} else {
+						// the password was correct and the results are shown in the contents widget
 						if !strings.HasPrefix(strings.Trim(msg, ""), "[sudo]") {
 							output = output + "\n" + msg
 							t.contents.SetText(output)
@@ -122,13 +238,16 @@ func (t *Tui) handle_sudo_cmd() {
 	}()
 }
 
-func execCmd(c chan string, command string) {
+// execute command and return the combined stderr and stdout via pipe to a channel. Channel is closed at the end to prevend a deadlock
+func execCmd(c chan string, command string, mode string) {
 	cmd := exec.Command("bash", "-c", command)
 
 	// create a pipe for stdout
 	stdout, _ := cmd.StdoutPipe()
-	// combine outputs of stderr and stdout
-	cmd.Stderr = cmd.Stdout
+	if mode == "both" {
+		// combine outputs of stderr and stdout
+		cmd.Stderr = cmd.Stdout
+	}
 	cmd.Start()
 
 	scanner := bufio.NewScanner(stdout)
@@ -141,11 +260,12 @@ func execCmd(c chan string, command string) {
 	close(c)
 }
 
-// test documentation
+// Create the main application with a new tui
 func CreateApplication() *Tui {
 	return new(Tui)
 }
 
+// define key bindings for the tui
 func (t *Tui) Keybindings() {
 
 	t.app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
@@ -156,13 +276,11 @@ func (t *Tui) Keybindings() {
 			t.pages.SendToFront("flex")
 		case tcell.KeyRune:
 			switch event.Rune() {
-			// execute selected script
-			// only if file was selected
+			// close the app
 			case 'q':
 				t.app.Stop()
-
+			// execute command
 			case 'c':
-				// execute commend
 				go t.handle_sudo_cmd()
 			}
 		}
